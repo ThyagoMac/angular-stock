4c4bd690fe19a0e7711d5fd52eaca321cff90ece
thyagomac <thyago.cormick@gmail.com>
2024-10-29 09:38:13 -0300

    cookieservice installed and implemented in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,7 @@ import { SignupComponent } from './modules/home/signup/signup.component';
     ButtonModule,
     ToastModule,
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
